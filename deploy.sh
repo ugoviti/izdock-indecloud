1c1f7d19054b27c361509e06f0d554f42a2d19f6
Ugo Viti <ugo.viti@initzero.it>
2019-12-24 16:38:56 +0100

    nodejs-inde: rimosso docker compose

--- deploy.sh
+++
@@ -1,6 +0,0 @@
-mkdir -p data/cloud-connector data/inde-self conf/cloud-connector conf/inde-self
-chown -R 993:993 data/cloud-connector data/inde-self conf/cloud-connector conf/inde-self
-docker-compose up -d 
-
-#docker volume create portainer_data
-#docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
